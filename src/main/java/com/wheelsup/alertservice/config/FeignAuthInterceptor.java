5fa4b6cadba835b4d5beacd45a5c4728cc23d846
Chintan Shah <chintan.shah@cshah-MacBook-Pro.local>
2021-03-26 11:10:07 -0400

    DR-333 cleanups as per code review

--- src/main/java/com/wheelsup/alertservice/config/FeignAuthInterceptor.java
+++ src/main/java/com/wheelsup/alertservice/config/FeignAuthInterceptor.java
@@ -1,21 +1,21 @@
 package com.wheelsup.alertservice.config;
 
-import com.wheelsup.alertservice.resource.APIAuthTokenResource;
 import com.wheelsup.alertservice.util.WuSecurityUtils;
+import com.wheelsup.common.util.AuthUtil;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class FeignAuthInterceptor implements RequestInterceptor {
-    private final APIAuthTokenResource apiAuthTokenResource;
+
     @Override
     public void apply(RequestTemplate template) {
-        template.header(HttpHeaders.AUTHORIZATION, WuSecurityUtils.buildBearerAuthorization(apiAuthTokenResource.getAccessToken()));
+        template.header(HttpHeaders.AUTHORIZATION, WuSecurityUtils.buildBearerAuthorization(AuthUtil.getToken(null)));
         template.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
         template.header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
     }
